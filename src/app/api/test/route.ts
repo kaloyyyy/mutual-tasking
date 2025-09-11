9b3c3393de90eafd895cb1d3ca097760a0a71f6a
kaloyyyy <kaloyau16@gmail.com>
2025-09-11 10:58:54 +0800

    added api testing

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,19 @@
+// /app/api/test/route.ts
+import { createClient } from "@/utils/supabase/client";
+
+export async function GET() {
+  try {
+    const supabase = createClient();
+    const { data, error } = await supabase.from("projects").select("*");
+
+    if (error) {
+      return new Response(JSON.stringify({ error: error.message }), {
+        status: 500,
+      });
+    }
+
+    return new Response(JSON.stringify({ data }), { status: 200 });
+  } catch (err: any) {
+    return new Response(JSON.stringify({ error: err.message }), { status: 500 });
+  }
+}
